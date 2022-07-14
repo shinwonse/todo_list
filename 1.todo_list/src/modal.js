06caa244767271929788091d636952197219abe8
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-07-14 21:30:08 +0900

    svg 파일 추가

---
+++ 1.todo_list/src/modal.js
@@ -0,0 +1,35 @@
+import 'styles/modal.css';
+import closeButton from './assets/delete.png';
+import editButton from './assets/pencil.svg';
+import deleteButton from './assets/trash.svg';
+
+export const modal = `
+  <div id='modal-container'>
+    <div id='modal-box'>
+      <button class='close-button'>
+        <img src=${closeButton} alt='close'/>
+      </button>
+      <div id='button-container'>
+        <button>
+          <img src=${editButton} alt='edit' class='modal-button-img'/>
+          <p>수정</p>
+        </button>
+        <button>
+          <img src=${deleteButton} alt='delete'    class='modal-button-img'/>
+          <p>삭제</p>
+        </button>
+      </div>
+    </div>
+  </div>
+`
+
+export const closeModal = ({ target }) => {
+  const modal = document.querySelector('.show-modal');
+  if (!target.closest('button')) {
+    return;
+  }
+  modal.classList.remove('show-modal');
+}
+
+
+

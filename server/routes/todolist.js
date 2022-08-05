317487bd85c9268b1285be11c43b0d6a5323ba56
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-08-05 17:13:36 +0900

    heroku test

--- server/routes/todolist.js
+++ server/routes/todolist.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('hello todolist')
+  res.send('hello todolist hahahaha')
 })
 
 router.post('/:id', (req, res) => {
